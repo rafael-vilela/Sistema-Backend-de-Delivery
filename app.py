c46e32407b724b8f34a108109031d8fb6be6c433
rafael-vilela <rafavpereira1@gmail.com>
2024-06-25 23:46:41 -0300

    Versão 1.0.1
    Nessa versão as 2 primeiras opções estão funcionais

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Pizzaria 1', 'Hamburgueria A']
+
 def exibir_nome():
     print('R̷a̷f̷a̷e̷l̷ D̷e̷l̷i̷v̷e̷r̷y̷\n')
 
@@ -13,20 +15,45 @@ def finalizar_app():
     os.system('cls')
     print('Encerrando o app...\n')
 
+def opcao_invalida():
+    input('Digite uma tecla para voltar ao inicio')
+    main()
+
+def cadastro_novo_restaurante():
+    os.system('cls')
+    print('Cadastro de novos restaurantes\n')
+    nome_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_restaurante)
+    print(f'O restaurante {nome_restaurante} foi cadastrado com sucesso!')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
+def listar_restaurantes():
+    os.system('cls')
+    print ('Listando restaurantes...\n')
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+    input('\nDigite uma tecla para voltar ao menu principal')
+    main()
+
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma das opções acima: '))
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurantes')
-    elif opcao_escolhida == 4:
-        finalizar_app()
-    else:
-        print('Opção inválida')
+    try:
+        opcao_escolhida = int(input('Escolha uma das opções acima: '))
+        if opcao_escolhida == 1:
+            cadastro_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            print('Ativar restaurantes')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()   
 
 def main():
+    os.system('cls')
     exibir_nome()
     exibir_opcoes()
     escolher_opcao()
